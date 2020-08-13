4a390e73bdbfb45077857581cd7ec6d73da3607d
Brendon Machado <benmacario.s@gmail.com>
2020-08-13 18:26:34 -0300

    jwt authentication

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'fad2660d395ddef950c1014b78f2fe2c',
+  expiresIn: '7d',
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
 const route = new Router();
 
 route.post('/user', UserController.store);
+route.post('/session', SessionController.store);
 
 export default route;
