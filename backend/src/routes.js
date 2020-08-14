a2a1ae9d036ed3e50076d8d5b2a745f7271ffa0e
Brendon Machado <benmacario.s@gmail.com>
2020-08-14 16:49:47 -0300

    Authentication  Middlewares

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,10 +2,15 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+import authMiddleware from './app/middlewares/auth';
 
 const route = new Router();
 
 route.post('/user', UserController.store);
 route.post('/session', SessionController.store);
+
+route.use(authMiddleware);
+
+route.put('/users', UserController.update);
 
 export default route;
