5287707fd083b310de6976524209bd473d51a933
Brendon Machado <benmacario.s@gmail.com>
2020-08-19 19:11:46 -0300

    config uploads with multer

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,21 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const route = new Router();
+const upload = multer(multerConfig);
 
 route.post('/user', UserController.store);
 route.post('/session', SessionController.store);
+
+route.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ status: 'OK' });
+});
 
 route.use(authMiddleware);
 
